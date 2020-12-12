e57339bcc35e03783c9fead3aa300ff28db11bcd
Nate Micinski <nmicinski@gmail.com>
2020-12-12 09:28:32 -0800

    setup initial directory structure

---
+++ src/pages/AllListings/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AllListings = () => {
+    return (<h1>all listings</h1>);
+}
+
+export default AllListings
---
+++ src/pages/ListingDetail/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ListingDetail = () => {
+  return <h1>listing detail</h1>;
+};
+
+export default ListingDetail;
