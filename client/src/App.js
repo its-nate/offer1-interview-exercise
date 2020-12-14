8968ad679946f881bcf777877f07a38ff5943783
Nate Micinski <nmicinski@gmail.com>
2020-12-13 19:41:12 -0800

    pull old data management out of App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,60 +1,25 @@
-import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import AllListings from "./pages/AllListings";
 import ListingDetail from "./pages/ListingDetail";
 import "./App.css";
-let API = require("./utils/API");
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listings: [],
-    };
-  }
-
-  getListings = () => {
-    API.listings().then((res) => {
-      // console.log(res);
-      this.setState({
-        listings: res,
-      });
-    });
-
-    // console.log(this.state);
-  };
-
-  getListing = (id) => {
-    for (let i = 0; i < this.state.listings.length; i++) {
-      if (this.state.listings[i].id === parseInt(id)) {
-        return this.state.listings[i];
-      }
-    }
-  };
-
-  componentDidMount() {
-    this.getListings();
-  }
-
-
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route exact path="/listings">
-            <AllListings listings={this.state.listings} />
-          </Route>
-          <Route path="/listings/:id">
-            <ListingDetail getListing={this.getListing} />
-          </Route>
-        </Switch>
-      </Router>
-    );
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/listings">
+          <AllListings listings={this.state.listings} />
+        </Route>
+        <Route path="/listings/:id">
+          <ListingDetail getListing={this.getListing} />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
